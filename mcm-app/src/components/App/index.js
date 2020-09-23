291f4034fdb2bb1d233bb91f36472a3d9a968f7c
Isaac Asante <isaac.editor16@gmail.com>
2020-09-24 04:15:18 +0800

    Added a Sidebar component to show the left menu pane on the front-end.

--- mcm-app/src/components/App/index.js
+++ mcm-app/src/components/App/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Navigation from "../Navigation";
+import Sidebar from "../Sidebar";
 
 // CSS
 import "../../template/lib/bootstrap/css/bootstrap.min.css";
@@ -12,6 +13,7 @@ import "../../template/css/style-responsive.css";
 const App = () => (
   <Router>
     <Navigation />
+    <Sidebar />
   </Router>
 );
 
