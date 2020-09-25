d491e3b2abb4ae0b2f3c70a0f31789d0f02849f3
Isaac Asante <isaac.editor16@gmail.com>
2020-09-25 09:38:18 +0800

    The routes were wrongly added to the roles.js file. Fixed.

--- mcm-app/src/constants/roles.js
+++
@@ -1,7 +0,0 @@
-export const LANDING = '/';
-export const SIGN_UP = '/register';
-export const SIGN_IN = '/login';
-export const DASHBOARD = '/dashboard';
-export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
-export const PASSWORD_FORGET = '/reset-password';
