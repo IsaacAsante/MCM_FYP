a6352c45323577fec01a23d93f633402b2787c05
Isaac Asante <isaac.editor16@gmail.com>
2020-09-29 08:13:29 +0800

    Created the SignUpForm class component, along with a SignUpLink module.

--- mcm-app/src/components/SignUp/index.js
+++ mcm-app/src/components/SignUp/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import * as ROUTES from "../../constants/routes";
 
 const SignUpPage = () => (
-    <div>
-        <section id="main-content">
-            <section className="wrapper">
-                <h1>SignUp component</h1>
-            </section>
-        </section>
-    </div>
+  <div>
+    <section id="main-content">
+      <section className="wrapper">
+        <h1>SignUp component</h1>
+      </section>
+    </section>
+  </div>
 );
 
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onSubmit = (event) => {};
+
+  onChange = (event) => {};
+
+  render() {
+    return <form onSubmit={this.onSubmit}></form>;
+  }
+}
+
+const SignUpLink = () => {
+  <p>
+    <Link to={ROUTES.SIGN_UP}>Register new user</Link>
+  </p>;
+};
+
 export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
