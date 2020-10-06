2b13d302e0bf4962d8c7c97c3710f1ad6c966ae0
Isaac Asante <isaac.editor16@gmail.com>
2020-10-06 20:33:51 +0800

    Bug fix for the navigation. The toggling button now works.

--- mcm-app/src/components/Navigation/index.js
+++ mcm-app/src/components/Navigation/index.js
@@ -3,11 +3,6 @@ import SignInButton from "../SignIn/button";
 import SignOutButton from "../SignOut";
 
 const Navigation = ({ authUser }) => (
-  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
-);
-
-// Navigation rendered when the user is logged in
-const NavigationAuth = () => (
   <div>
     {/* <!--header start--> */}
     <header className="header black-bg">
@@ -24,33 +19,7 @@ const NavigationAuth = () => (
           MCM<span> System</span>
         </b>
       </a>
-      {/* <!--logo end--> */}
-      <SignOutButton />
-    </header>
-    {/* <!--header end--> */}
-  </div>
-);
-
-// Navigation rendered when the user is not logged in.
-const NavigationNonAuth = () => (
-  <div>
-    {/* <!--header start--> */}
-    <header className="header black-bg">
-      <div className="sidebar-toggle-box">
-        <div
-          className="fa fa-bars tooltips"
-          data-placement="right"
-          data-original-title="Toggle Navigation"
-        ></div>
-      </div>
-      {/* <!--logo start--> */}
-      <a href="index.html" className="logo">
-        <b>
-          MCM<span> System</span>
-        </b>
-      </a>
-      {/* <!--logo end--> */}
-      <SignInButton />
+      {authUser ? <SignOutButton /> : <SignInButton />}
     </header>
     {/* <!--header end--> */}
   </div>
