51547718f42f7c24689ec05e96fb3ff684d83859
Isaac Asante <isaac.editor16@gmail.com>
2020-10-08 08:59:49 +0800

    Minor changes to the Account component with AuthUserContext.

--- mcm-app/src/components/Account/index.js
+++ mcm-app/src/components/Account/index.js
@@ -2,30 +2,34 @@ import React from "react";
 
 import { PasswordResetForm } from "../PasswordReset";
 import { PasswordUpdateForm } from "../PasswordUpdate";
-import { withAuthorization } from "../Session";
+import { withAuthorization, AuthUserContext } from "../Session";
 
 const AccountPage = () => (
-  <div>
-    <section id="main-content">
-      <section className="wrapper">
-        <h1>
-          <i className="fa fa-angle-right"></i> Your Account
-        </h1>
-        <div className="row mt">
-          <div className="col-lg-12">
-            <div className="form-panel">
-              <h3>Update Password</h3>
-              <hr />
-              <PasswordUpdateForm />
-              <h3>Reset Password</h3>
-              <hr />
-              <PasswordResetForm />
+  <AuthUserContext.Consumer>
+    {(authUser) => (
+      <div>
+        <section id="main-content">
+          <section className="wrapper">
+            <h1>
+              <i className="fa fa-angle-right"></i> Your Account
+            </h1>
+            <div className="row mt">
+              <div className="col-lg-12">
+                <div className="form-panel">
+                  <h3>Update Password</h3>
+                  <hr />
+                  <PasswordUpdateForm />
+                  <h3>Reset Password</h3>
+                  <hr />
+                  <PasswordResetForm />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
-    </section>
-  </div>
+          </section>
+        </section>
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
 const condition = (authUser) => !!authUser;
