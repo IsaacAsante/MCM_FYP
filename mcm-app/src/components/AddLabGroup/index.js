8f4d52495e3a76f06b3bd79a1a679943a907cb43
Isaac Asante <isaac.editor16@gmail.com>
2020-11-07 10:05:58 +0800

    The tutor's first name and last name can now be retrieved from Excel file sheets during the import process.

--- mcm-app/src/components/AddLabGroup/index.js
+++ mcm-app/src/components/AddLabGroup/index.js
@@ -11,6 +11,7 @@ import * as ROUTES from "../../constants/routes";
 const INITIAL_STATE = {
   error: null,
   sheets: [],
+  success: false,
   tutor: {},
   workbook: null,
 };
@@ -43,66 +44,83 @@ class AddLabGroupPage extends React.Component {
     event.preventDefault();
     const data = new FormData();
     data.append("file", this.state.workbook);
-    axios.post("/reader", data, {}).then((res) => {
-      console.log(res.data);
-      // Main array to check for dups
-      let tracker = [];
-      let importData = {};
-      for (const [k, v] of Object.entries(res.data)) {
-        importData[k] = {};
-        this.props.firebase
-          .findTutor("Tutor", "Lectiurer")
-          .then((res) => {
-            if (!res) throw "This tutor was not found in the DB";
-            importData[k]["tutorEmail"] = res.email;
-            return importData;
-          })
-          .then((importData) => {
-            let group = [];
-            // Get Lab data
-            const lab_time_details = v["C3"].w.split(/[\s,]/);
-            const lab_day = DAY[lab_time_details[0]];
-            const lab_time = lab_time_details[2];
-            importData[k]["time"] = lab_time;
-            importData[k]["weekDay"] = lab_day;
-            importData[k]["offeringID"] = this.props.match.params.offeringID;
-            // console.log("Lab detail:", importData);
-            let student_IDs = [];
-            for (const [k1, v1] of Object.entries(v)) {
-              if (k1 >= "B" && k1 < "C") {
-                // Only process the student data if the type of data in that B cell is of type number
-                // If the type is number, then it refers to a student ID.
-                if (v1.t == "n") {
-                  let row = k1.substring(1);
-                  const student = {
-                    id: v1.w,
-                    name: v[`C${row}`].w,
-                  };
-                  // Look out for duplicate student entries (Lectures + Lab)
-                  if (!tracker.includes(student.id)) {
-                    student_IDs.push(student.id);
-                    group.push(student);
-                    tracker.push(student.id);
+    axios
+      .post("/reader", data, {})
+      .then((res) => {
+        console.log(res.data);
+        // Main array to check for dups
+        let tracker = [];
+        let importData = {};
+        for (const [k, v] of Object.entries(res.data)) {
+          let staff = v["C5"].w;
+          const tutorName = staff.substring(7).split(" ");
+          const tutorFirstName = tutorName.shift();
+          const tutorLastName = tutorName.join(" ");
+          importData[k] = {};
+          this.props.firebase
+            .findTutor(tutorFirstName, tutorLastName)
+            .then((res) => {
+              if (!res)
+                throw `Operation aborted. The tutor ${tutorFirstName} ${tutorLastName} was not found in the database. Make sure they have an account in the system first before importing this file.`;
+              importData[k]["tutorEmail"] = res.email;
+              return importData;
+            })
+            .then((importData) => {
+              let group = [];
+              // Get Lab data
+              const lab_time_details = v["C3"].w.split(/[\s,]/);
+              const lab_day = DAY[lab_time_details[0]];
+              const lab_time = lab_time_details[2];
+              importData[k]["time"] = lab_time;
+              importData[k]["weekDay"] = lab_day;
+              importData[k]["offeringID"] = this.props.match.params.offeringID;
+              // console.log("Lab detail:", importData);
+              let student_IDs = [];
+              for (const [k1, v1] of Object.entries(v)) {
+                if (k1 >= "B" && k1 < "C") {
+                  // Only process the student data if the type of data in that B cell is of type number
+                  // If the type is number, then it refers to a student ID.
+                  if (v1.t == "n") {
+                    let row = k1.substring(1);
+                    const student = {
+                      id: v1.w,
+                      name: v[`C${row}`].w,
+                    };
+                    // Look out for duplicate student entries (Lectures + Lab)
+                    if (!tracker.includes(student.id)) {
+                      student_IDs.push(student.id);
+                      group.push(student);
+                      tracker.push(student.id);
+                    }
+                    // console.log(`${k1}: ${v1.w}, C${row}: ${v[`C${row}`].w}`);
                   }
-                  // console.log(`${k1}: ${v1.w}, C${row}: ${v[`C${row}`].w}`);
                 }
               }
-            }
-            console.log(group);
-            importData[k]["students"] = student_IDs; // Maintain the lab names from the imported file
-          })
-          .catch((err) => {
-            console.log(err);
-            this.setState({
-              error: "An error occurred. Please retry again later.",
+              // console.log(group);
+              importData[k]["students"] = student_IDs; // Maintain the lab names from the imported file
+            })
+            .catch((error) => {
+              console.log(error);
+              this.setState({
+                error,
+                success: false,
+              });
             });
-          });
-      }
-      console.log("Import Data:", importData);
-    });
+        }
+        this.setState({ ...INITIAL_STATE });
+        this.setState({ success: true });
+        console.log("Import Data:", importData);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          error: "Something went wrong. Please retry again later.",
+        });
+      });
   };
 
   render() {
+    const { error, success } = this.state;
     return (
       <div>
         <section id="main-content">
@@ -134,6 +152,19 @@ class AddLabGroupPage extends React.Component {
             >
               Save
             </button>
+            {!success ? (
+              error ? (
+                <div className="alert alert-danger mt">
+                  <span>{error}</span>
+                </div>
+              ) : (
+                ""
+              )
+            ) : (
+              <div className="alert alert-success mt">
+                <span>File read successfully.</span>
+              </div>
+            )}
           </form>
         </section>
       </div>
